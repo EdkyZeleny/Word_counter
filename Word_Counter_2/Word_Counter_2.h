bca0e4fe87254077407afc7df08ce0edf9a6a455
EdkyZeleny <49240143+EdkyZeleny@users.noreply.github.com>
2019-12-02 03:14:47 +0500

    Update Word_Counter_2.h

--- Word_Counter_2/Word_Counter_2.h
+++ Word_Counter_2/Word_Counter_2.h
@@ -11,12 +11,8 @@
 #include <string>
 
 
-#ifndef EXAMPLEPROJECT_FORMULA_H
-#define EXAMPLEPROJECT_FORMULA_H
-
 class Word_Counteringer {
 public:
 	static int Counting(std::string arg1);
 };
 
-#endif //EXAMPLEPROJECT_FORMULA_H
