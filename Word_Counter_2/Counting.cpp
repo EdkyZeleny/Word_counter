2a9bfe7829c9af3407d7662e1018e4473b2bf266
EdkyZeleny <49240143+EdkyZeleny@users.noreply.github.com>
2019-12-02 03:16:19 +0500

    Update Counting.cpp

--- Word_Counter_2/Counting.cpp
+++ Word_Counter_2/Counting.cpp
@@ -1,36 +1,36 @@
 #include "Word_Counter_2.h"
 using namespace std;
 
-int Word_Counteringer::Counting( std::string arg1) {
+int Word_Counteringer::Counting(std::string arg1) {
+  ifstream file_stream;
+  file_stream.open(arg1);
 
-	ifstream file_stream;
-	file_stream.open(arg1);
+  if (!file_stream.is_open()) {
+    cout << "ÃÃ¥ Ã³Ã¤Ã Ã«Ã®Ã±Ã¼ Ã®Ã²ÃªÃ°Ã»Ã²Ã¼ Ã´Ã Ã©Ã«. ÃÃ°Ã®Ã¢Ã¥Ã°Ã¼Ã²Ã¥ Ã¯Ã°Ã Ã¢Ã¨Ã«Ã¼Ã­Ã®Ã±Ã²Ã¼ Ã¢Ã¢Ã®Ã¤Ã ." << endl;
+    return 1;
+  }
 
-	if (!file_stream.is_open()) {
-		cout << "Íå óäàëîñü îòêðûòü ôàéë. Ïðîâåðüòå ïðàâèëüíîñòü ââîäà." << endl;
-		return 1;
-	}
+  map<string, size_t> word_to_count;
 
-	map<string, size_t> word_to_count;
+  string word;
+  while (file_stream >> word) {
+    ++word_to_count[word];
+  }
 
-	string word;
-	while (file_stream >> word) {
-		++word_to_count[word];
-	}
+  vector<pair<string, size_t>> stats(word_to_count.begin(),
+                                     word_to_count.end());
 
-	vector<pair<string, size_t>> stats(word_to_count.begin(),
-		word_to_count.end());
+  sort(stats.begin(), stats.end(), [](auto& lhs, auto& rhs) {
+    if (lhs.second == rhs.second) {
+      return lhs.first < rhs.first;
+    }
+    return lhs.second > rhs.second;
+  });
 
-	sort(stats.begin(), stats.end(), [](auto& lhs, auto& rhs) {
-		if (lhs.second == rhs.second) {
-			return lhs.first < rhs.first;
-		}
-		return lhs.second > rhs.second;
-	});
+  for (auto& pair : stats) {
+    cout << "word: " << pair.first << " is used " << pair.second << " times"
+         << endl;
+  }
 
-	for (auto& pair : stats) {
-		cout << "word: " << pair.first << " is used " << pair.second << " times" << endl;
-	}
-	
-	return 1;
+  return 1;
 }
