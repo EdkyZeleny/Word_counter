38719199a12479e55aba65162a8ac55d5c775876
EdkyZeleny <49240143+EdkyZeleny@users.noreply.github.com>
2019-12-02 06:23:47 +0500

    Update Word_Counter_2.cpp

--- Word_Counter_2/Word_Counter_2.cpp
+++ Word_Counter_2/Word_Counter_2.cpp
@@ -7,7 +7,7 @@ string path;  //"C:\\rutext.txt"
 int main() {
   setlocale(LC_CTYPE, "rus");
 
-  cout << "Input the file source like C:\rutext.txt" << endl;
+  cout << "Please, input there file source like C:\rutext.txt" << endl;
   cin >> path;
 
   Word_Counteringer::Counting(path);
